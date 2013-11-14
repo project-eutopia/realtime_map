0da2aabc3ea37b840dda8064f396315050484dc8
Chris Locke <clocke@degica.com>
2013-11-14 11:02:34 +0900

    First commit of new rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RealtimeMap::Application.config.secret_key_base = 'f5ff76448fae8bd2bdf383023c7062c83d02e383af227342db7676aed59203dc46e7e101b7446b33d9cc465c4e13268679b722aaacebd1020b1acd981f4baede'
